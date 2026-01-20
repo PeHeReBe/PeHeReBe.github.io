1a880f3007435dccd202d2f9b138490ed9416cc6
PeHeReBe <54329944+PeHeReBe@users.noreply.github.com>
2026-01-20 18:30:04 +0100

    Update ark-config.js

--- server/ark-config.js
+++ server/ark-config.js
@@ -13,6 +13,12 @@ const serverConfig = {
             disabled: true,
             style: 'opacity: 0.6;'
         },
+       {
+            type: 'secondary',
+            text: 'Server Panel',
+            icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>',
+            href: 'https://pelican.afkingdom.de/server/fdf641be'
+        },
         {
             type: 'secondary',
             text: 'Discord beitreten',
