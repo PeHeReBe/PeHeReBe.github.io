89feffd5605cc50c378edc13a0bbfd58e513d4a0
PeHeReBe <54329944+PeHeReBe@users.noreply.github.com>
2026-01-20 19:19:56 +0100

    Enable Server IP copy button for ARK server

--- server/ark-config.js
+++ server/ark-config.js
@@ -8,10 +8,9 @@ const serverConfig = {
     buttons: [
         {
             type: 'primary',
-            text: 'Bald verfügbar',
-            icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>',
-            disabled: true,
-            style: 'opacity: 0.6;'
+            text: 'Server IP kopieren',
+            icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg>',
+            action: 'copyIP'
         },
        {
             type: 'secondary',
